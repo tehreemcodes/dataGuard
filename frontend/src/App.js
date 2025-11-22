189ce6b118d7339c8ffeb7d129dd05398fbd60af
tehreemcodes <tehreem.zafar06@gmail.com>
2025-11-22 13:49:25 +0500

    feat: added routing structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import UploadDataset from "./pages/UploadDataset";
+import CreatePolicy from "./pages/CreatePolicy";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/upload" element={<UploadDataset />} />
+        <Route path="/policy" element={<CreatePolicy />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
